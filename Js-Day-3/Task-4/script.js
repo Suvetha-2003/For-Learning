247b3f800dd5528e670cf6a7959fdf4fde9240cc
Suvetha <suvethavelusamy697@gmail.com>
2025-01-10 15:43:33 +0530

    commit the changes

--- Js-Day-3/Task-4/script.js
+++ Js-Day-3/Task-4/script.js
@@ -1,13 +1,10 @@
 function Remove() {
 const s=document.getElementById("inp1").value;
     let result = ""; 
-
-    for (let i = 0; i < s.length; i++) {
-        if (result.indexOf(s[i]) === -1) {
-            result += s[i];
+    for (let i=0;i<s.length;i++) {
+        if (result.indexOf(s[i])=== -1) {
+            result+=s[i];
         }
     }
-
     console.log(result);
 }
-
