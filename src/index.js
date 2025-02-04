9a1fb9cdd474693fc418890ca526ec8a90a0b411
Suvetha <suvethavelusamy697@gmail.com>
2025-02-04 11:29:12 +0530

    commit the react

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Task1 from './Components/Task-1/Task1';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Task1 />
+  </React.StrictMode>
+);
